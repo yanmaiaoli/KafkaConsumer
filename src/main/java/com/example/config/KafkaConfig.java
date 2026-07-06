b37bb821a02d0544b4e6a063cf816d0f63fccc66
yanmaiaoli <yanmaiaoli@gmail.com>
2026-07-06 20:56:44 -0300

    config id da maquina

--- src/main/java/com/example/config/KafkaConfig.java
+++ src/main/java/com/example/config/KafkaConfig.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 
 public class KafkaConfig {
     
-    private static final String BOOTSTRAP_SERVERS = "172.27.240.1:19092,172.27.240.1:29092,172.27.240.1:39092";
+    private static final String BOOTSTRAP_SERVERS = "172.23.168.107:19092,172.23.168.107:29092,172.23.168.107:39092";
     public static final String AUDIO_TOPIC = "audio-monitoring";
     public static final String LEQ_ALERT_TOPIC = "alert_leq";
     public static final String HIGH_AND_LOWS_TOPIC = "alert_high_and_lows";
@@ -18,7 +18,7 @@ public class KafkaConfig {
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1000);
-        props.put("auto.offset.reset", "latest");
+        props.put("auto.offset.reset", "earliest");
         return props;
     }
 }
